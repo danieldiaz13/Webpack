1d2e27a93a44b781eed45eb0a88d3014e654239c
daniel diaz <danieldiaz132001@gmail.com>
2021-09-14 10:29:28 -0500

    todo

---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const DotEnv = require('dotenv-webpack')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+    ntry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    assetModuleFilename: 'assets/img/[hash][ext][query]'
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin()
+    ]
+  },
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      '@img': path.resolve(__dirname, './src/assets/img'),
+      '@css': path.resolve(__dirname, './src/assets/css'),
+      '@scss': path.resolve(__dirname, './src/assets/scss')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.s?css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.png/,
+        type: 'asset/resource'
+      }
+    ]
+  }, 
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true, 
+      template: './src/index.html',
+      filename: './index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: './assets/css/[name].[contenthash].css'
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src', './assets/img'),
+          to: './assets/img'
+        }
+      ]
+    }),
+    new DotEnv(),
+    new CleanWebpackPlugin()
+  ]
+}
