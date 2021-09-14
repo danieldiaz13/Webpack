4befa360eb309056be7633a53ce78a73e47cb4f5
daniel diaz <danieldiaz132001@gmail.com>
2021-09-14 10:53:48 -0500

    t1

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,32 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const CopyPlugin = require('copy-webpack-plugin')
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
-const DotEnv = require('dotenv-webpack')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const DotEnv = require('dotenv-webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    ntry: './src/index.js',
+  // Entry nos permite decir el punto de entrada de nuestra aplicación
+  entry: './src/index.js',
+  // Output nos permite decir hacia dónde va enviar lo que va a preparar webpacks
   output: {
+    // path es donde estará la carpeta donde se guardará los archivos
+    // Con path.resolve podemos decir dónde va estar la carpeta y la ubicación del mismo
     path: path.resolve(__dirname, 'dist'),
+    // filename le pone el nombre al archivo final
     filename: '[name].[contenthash].js',
-    assetModuleFilename: 'assets/img/[hash][ext][query]'
-  },
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new CssMinimizerPlugin()
-    ]
+    assetModuleFilename: 'assets/images/[hash][ext][query]'
   },
   resolve: {
+    // Aqui ponemos las extensiones que tendremos en nuestro proyecto para webpack los lea
     extensions: ['.js'],
     alias: {
-      '@img': path.resolve(__dirname, './src/assets/img'),
-      '@css': path.resolve(__dirname, './src/assets/css'),
-      '@scss': path.resolve(__dirname, './src/assets/scss')
+      '@utils': path.resolve(__dirname, 'src/utils/'),
+      '@templates': path.resolve(__dirname, 'src/templates/'),
+      '@styles': path.resolve(__dirname, 'src/styles/'),
+      '@images': path.resolve(__dirname, 'src/assets/images/'),
     }
   },
   module: {
@@ -37,37 +39,58 @@ module.exports = {
         }
       },
       {
-        test: /\.s?css$/i,
+        test: /\.css|.styl$/i,
         use: [
           MiniCssExtractPlugin.loader,
           'css-loader',
-          'sass-loader'
+          'stylus-loader'
         ]
       },
       {
-        test: /\.png/,
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource'
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+            mimetype: 'application/font-woff',
+            name: '[name].[contenthash].[ext]',
+            outputPath: './assets/fonts/',
+            publicPath: '../assets/fonts/',
+            esModule: false,
+          }
+        }
       }
     ]
-  }, 
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      inject: true, 
-      template: './src/index.html',
+      inject: true,
+      template: './public/index.html',
       filename: './index.html'
     }),
     new MiniCssExtractPlugin({
-      filename: './assets/css/[name].[contenthash].css'
+      filename: 'assets/[name][contenthash].css'
     }),
-    new CopyPlugin({
+    new CopyWebpackPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, 'src', './assets/img'),
-          to: './assets/img'
+          from: path.resolve(__dirname, 'src/', 'assets/images'),
+          to: 'assets/images'
         }
       ]
     }),
     new DotEnv(),
     new CleanWebpackPlugin()
-  ]
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin(),
+      new TerserPlugin(),
+    ]
+  }
 }
